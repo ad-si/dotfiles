5371211cd734940eb4ff5bbf5b64cf46e0dd09d2
Adrian Sieber <mail@adriansieber.com>
2017-01-31 10:38:01 +0000

    Improve order of paths in $PATH variable

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,8 +1,14 @@
-set -x PATH ~/dotfiles/bin $PATH
-set -x PATH /usr/local/sbin $PATH
-set -x GOPATH $HOME/.go
-set -x PATH $GOPATH/bin $PATH
-set -x PATH ~/.gem/ruby/2.0.0/bin $PATH
+set -x GOPATH $HOME/.go $GOPATH
+set -x OSA_LIBRARY_PATH $HOME/dotfiles/script-libraries $OSA_LIBRARY_PATH
+
+# Add paths to path variable with increasing priority
+set -x PATH /usr/local/sbin $PATH                                       # User
+set -x PATH (brew --prefix homebrew/php/php71)/bin $PATH                # PHP
+set -x PATH /Applications/Postgres.app/Contents/Versions/9.4/bin $PATH  # Postgr
+set -x PATH $GOPATH/bin $PATH                                           # Go
+set -x PATH (brew --prefix ruby)/bin $PATH                              # Ruby
+set -x PATH /usr/local/lib/node_modules $PATH                           # Npm
+set -x PATH ~/dotfiles/bin $PATH                                        # Custom
 
 
 [ -f /usr/local/share/autojump/autojump.fish ];
