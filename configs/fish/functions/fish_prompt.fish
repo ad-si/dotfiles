610c5b628d9a3ccbb214821c55b07bd8df19d6f4
Adrian Sieber <mail@adriansieber.com>
2020-11-10 15:14:00 +0000

    Redesign CLI prompt

--- configs/fish/functions/fish_prompt.fish
+++ configs/fish/functions/fish_prompt.fish
@@ -1,4 +1,3 @@
-
 function printPagesPerDay
   set -l bookPath "/Users/adrian/Dropbox/Projects/programmiersprachenbuch/main.pdf"
   set -l writtenPages (pdfinfo "$bookPath" | grep Pages | cut -c 9-)
@@ -12,6 +11,26 @@ function printPagesPerDay
 end
 
 
+function printTasksStatus
+  if command -sq tasklite
+    set --local taskCount (tasklite runsql \
+      "select count(*) from tasks_view where closed_utc is null and tags is null" \
+      | tail -n 1)
+
+    set --local openTasks (tl count state:open)
+
+    # set --local taskDesc (tasklite next \
+    #   | grep '^body: ' \
+    #   | cut -c 7- \
+    #   | sed -E 's/(.{40})(.{1,})$/\1…/')
+
+    # printf ' %s | %s ' $taskCount # $taskDesc
+
+    printf ' 📥 %s | ☑️  %s ' $taskCount $openTasks
+  end
+end
+
+
 function fish_prompt --description 'Write out the prompt'
   set -l last_status $status
 
@@ -19,13 +38,18 @@ function fish_prompt --description 'Write out the prompt'
     set -g __fish_prompt_normal (set_color normal)
   end
 
+  if not test $last_status -eq 0
+    set_color $fish_color_error
+    printf '❌ '
+  end
+
   # Disable path shortening
   set -g fish_prompt_pwd_dir_length 0
 
-  set_color -b 000000 $fish_color_cwd
+  set_color --background 000000 $fish_color_cwd
   printf ' %s%s ' (prompt_pwd) (__fish_git_prompt)
 
-  set_color -b 333333 000000
+  set_color --background 333333 000000
   printf '\ue0b0'
 
   set_color cyan
@@ -37,55 +61,38 @@ function fish_prompt --description 'Write out the prompt'
   set_color red
   printf '%s ' (hostname | cut -d . -f 1)
 
-  set_color -b 000000 333333
+  # Gray Arrow End
+  set_color --background 000000 333333
   printf '\ue0b0'
-  set_color -b 000000
+  set_color --background 000000
 
   # Show datetime
   # set_color white
   # printf ' %s ' (date -u "+%Y-%m-%d %H:%M:%S")
 
-  # set_color -b 333333 000000
+  # set_color --background 333333 000000
   # printf '\ue0b0'
-  # set_color -b 333333
+  # set_color --background 333333
 
   set_color red
 
-  # if command -sq tasklite
-  #   set --local taskCount (tasklite runsql \
-  #     "select count(*) from tasks where state is 'Open'" \
-  #     | tail -n 1)
+  printTasksStatus
 
-  #   set --local taskDesc (tasklite next \
-  #     | grep '^body: ' \
-  #     | cut -c 7- \
-  #     | sed -E 's/(.{40})(.{1,})$/\1…/')
-
-  #   printf ' %s | %s ' $taskCount $taskDesc
-  # end
-
-  printPagesPerDay
+  # printPagesPerDay
 
   # Gray Arrow End
-  # set_color -b normal 333333
+  # set_color --background normal 333333
   # printf '\ue0b0'
   # set_color normal
 
   # Black Arrow End
-  set_color -b normal 000000
+  set_color --background normal 000000
   printf '\ue0b0'
   set_color normal
 
   printf '\n'
 
-  if not test $last_status -eq 0
-    set_color $fish_color_error
-  end
-
-  set_color -b 000000 magenta
-  printf '$'
-
-  set_color -b normal 000000
-  printf '\ue0b0 '
+  set_color magenta
+  printf '$ '
   set_color normal
 end
