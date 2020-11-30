4f69a586bd866063eececa386ccb4ea38c95b1bd
Adrian Sieber <mail@adriansieber.com>
2020-11-30 00:04:37 +0000

    Add Hammerspoon config, add MIDI shortcuts to open apps

---
+++ home/hammerspoon/init.lua
@@ -0,0 +1,34 @@
+hs.loadSpoon("Calendar")
+
+hs.loadSpoon("ClipboardTool")
+spoon.ClipboardTool:start()
+
+-- TODO: Add https://www.hammerspoon.org/Spoons/MusicAppMediaFix.html
+
+-- MIDI devices are normally
+-- 1 Network
+-- 2 Bluetooth
+-- 3 Attached MIDI client
+midiDevice = hs.midi.new(hs.midi.devices()[3])
+
+midiDevice:callback(
+  function(object, deviceName, commandType, description, metadata)
+    if commandType == "noteOn"
+    then
+      if metadata.note == 48 -- C2
+      then
+        hs.application.open("Brave Browser")
+      end
+
+      if metadata.note == 50 -- D2
+      then
+        hs.application.open("iTerm")
+      end
+
+      if metadata.note == 52 -- E2
+      then
+        hs.application.open("Sublime Text")
+      end
+    end
+  end
+)
