475600c92c624bee4d40a3a5f16fe8ce87ea7c15
Adrian Sieber <mail@adriansieber.com>
2021-02-25 13:34:26 +0000

    Minor config changes

---
+++ configs/fish/functions/less.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function less --wraps='less -R' --description 'alias less=less -R'
+ command less -R $argv;
+end
--- home/hammerspoon/init.lua
+++ home/hammerspoon/init.lua
@@ -9,26 +9,34 @@ spoon.ClipboardTool:start()
 -- 1 Network
 -- 2 Bluetooth
 -- 3 Attached MIDI client
-midiDevice = hs.midi.new(hs.midi.devices()[3])
+if hs.midi.devices()[3]
+then
+  midiDevice = hs.midi.new(hs.midi.devices()[3])
 
-midiDevice:callback(
-  function(object, deviceName, commandType, description, metadata)
-    if commandType == "noteOn"
-    then
-      if metadata.note == 48 -- C2
+  midiDevice:callback(
+    function(object, deviceName, commandType, description, metadata)
+      if commandType == "noteOn"
       then
-        hs.application.open("Brave Browser")
-      end
+        if metadata.note == 48 -- C2
+        then
+          hs.application.open("Brave Browser")
+        end
 
-      if metadata.note == 50 -- D2
-      then
-        hs.application.open("iTerm")
-      end
+        if metadata.note == 50 -- D2
+        then
+          hs.application.open("iTerm")
+        end
 
-      if metadata.note == 52 -- E2
-      then
-        hs.application.open("Sublime Text")
+        if metadata.note == 52 -- E2
+        then
+          hs.application.open("Sublime Text")
+        end
+
+        if metadata.note == 53 -- F2
+        then
+          hs.application.open("Thunderbird")
+        end
       end
     end
-  end
-)
+  )
+end
