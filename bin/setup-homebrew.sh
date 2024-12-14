bda492df75a1963a59001c326a100c9331ce3bee
Adrian Sieber <mail@adriansieber.com>
2024-12-14 16:13:03 +0000

    Homebrew setup: Add missing tools and apps to install

--- bin/setup-homebrew.sh
+++ bin/setup-homebrew.sh
@@ -39,58 +39,92 @@ brew install bash
 # Miscellaneous
 brews=(
   # ag
+  # aichat
+  # asciinema
+  # atuin
+  # awscli
   bat
   bun  # JavaScript engine
   # colordiff
   # csvkit  # Utilities for converting to and working with CSV
+  # deno  # JavaScript runtime
   diskus
   dockutil
-  eza
+  # doggo  # DNS lookup utility
+  # duckdb  # In-memory SQL database system
   exiftool  # Manage meta information of vaious files
+  eza
   fastfetch  # Display system information in the terminal
+  fclones  # Find duplicate files
   fd
-  fdupes
+  # fdupes  # Find duplicate files (Use fclones instead)
   fish
+  flyctl  # Fly.io CLI
   fnm  # Fast Node.js version manager
+  # fzf  # Fuzzy finder
   # gcal  # TODO: Re-add after the formula was re-activated on homebrew
   gh
   git
   git-extras
+  # gitbatch  # Tool to manage multiple git repositories
+  # gleam  # Gleam programming language
   gnu-sed
   # gnuplot  # A graph plotter with support for various formats
   # go  # Go programming language
+  # goaccess  # Web log analyzer
   gopass
   graphviz
+  # gum  # Utilities to write shell scripts
   hammerspoon  # Desktop automation
+  # hexyl  # Hex viewer
+  # heynote  # Note-taking app
   htop
   httpie
   # hunspell
+  # hurl  # HTTP client and testing tool
+  # hyperfine  # Benchmarking tool
   imagemagick
+  jo  # JSON output from a shell
+  jq  # JSON processor
   just  # Command runner
   kalker
   kondo  # Clean dependencies and build artifacts
   # kubectl
+  # lilypond  # Music notation software
+  lychee  # Link checker
   mas
+  mdbook # Create books from markdown files
   # micro
+  # mitmproxy  # Intercept, modify, replay, or save HTTP traffic
   # mongodb
-  # node
+  # mosh  # Mobile shell
   mpv  # Media player
+  # node
+  numbat  # Calculator with support for units
   # ocrmypdf  # Add an OCR text layer to scanned PDF files
   ollama
   # optipng
+  ocrmypdf  # Add an OCR text layer to scanned PDF files
+  optipng  # PNG optimizer
   # osx-cpu-temp
-  # pandoc
+  pandoc
   # pandoc-citeproc
+  # perspec  # Extract and perspectively correct document images
   pinentry-mac  # Needed for gopass
   pkg-config  # Necessary for stack
   # platypus  # CLI tool only (GUI gets installed with brew cask)
   # poppler
+  rclone  # Rsync for cloud storage
+  rga  # Ripgrep wrapper for PDFs, E-Books, Office documents, zip, tar.gz, etc.
   ripgrep
   # ruby  # Programming language
   # s3cmd  # Command-line tool for the Amazon S3 service
+  sd  # Intuitive find & replace CLI
   # shellcheck  # Linter for bash scripts
   stack  # Haskell tool stack
+  sqlite  # SQLite database
   tealdeer  # Fast implementation of tldr in Rust
+  # tectonic # LaTeX engine
   # tokei  # Display statistics about code
   topgrade
   trash
@@ -98,12 +132,16 @@ brews=(
   tree
   # tesseract  # OCR (Optical Character Recognition) tool
   # tesseract-lang  # All supported languages
+  typos-cli  # Find typos in text
+  typst  # Markup based typesetting system
   # uchardet  # Encoding detection library
   uv  # Python package manager
   viu  # Terminal image viewer
+  # vlang  # V programming language
   watchexec  # Executes commands in response to file modifications
+  wolfram-engine  # Execute Wolfram Language code
   xsv  # CSV command line toolkit written in Rust
-  # yarn
+  # zola  # Static site generator
   zoxide  # Jump to directories
 )
 
@@ -123,22 +161,29 @@ casks=(
   affinity-photo
   affinity-designer
   # alfred
-  # arduino
   # android-file-transfer
+  # angry-ip-scanner
+  # anki
+  # arduino
   # atom
   # automap-server FIXME: missing
   beekeeper-studio  # SQL Editor and Database Manager
   # betterzipql  # Preview archives  FIXME: missing
   brave-browser
+  bruno
   # blender
   # calibre
   # cheatsheet
   # chromium
-  # cloudcompare FIXME: missing
-  # db-browser-for-sqlite
-  # dedrm FIXME: missing
+  # cloudcompare  # FIXME: missing
+  # cyberduck
+  # db-browser-for-sqlite@nightly
+  # dedrm  # FIXME: missing
+  # defold  # Game engine with Lua scripting
+  # diffusionbee
   discord
   # disk-inventory-x
+  docker
   dropbox
   # duet
   # electron-cash
@@ -153,23 +198,30 @@ casks=(
   # franz
   # fritzing
   font-hasklug-nerd-font
+  # font-liberation
+  # forklift  # File manager
+  # frescobaldi  # LilyPond music notation editor
   gimp
   # gitup
   godot  # Game engine
   # google-chrome
   # google-cloud-sdk
+  # gramps  # Genealogy software
   # handbrake
   # hex-fiend
   # homebrew/cask-versions/microsoft-remote-desktop-beta
-  homebrew/cask/docker
   # ichm
   # imageplay
   # inkscape
   iterm2
+  # jjazzlab  # Jazz music software
+  # jupyterlab
   # kindle
   # kindlegen
+  krita  # Digital painting
   # librecad
   # libreoffice
+  # lm-studio
   logitech-options
   mas  # Mac App Store command line interface
   # microsoft-remote-desktop
@@ -193,13 +245,16 @@ casks=(
   # slack
   # slic3r
   # squeak FIXME: missing
+  # solvespace  # Parametric 2D/3D CAD
   # sourcetree
   # speedcrunch    #  Calculator
   spotify
   sublime-merge
   sublime-text
+  # sweet-home3d  # Interior design software
   # table-tool
   # tad
+  # teamviewer  # Remote desktop software
   # teensy
   telegram
   # texstudio
@@ -215,12 +270,13 @@ casks=(
   # vmware-fusion
   whatsapp
   xquartz
+  # zed  # Text and code editor
   zoom  # Video conferencing
 )
 
 for cask in "${casks[@]}"
 do
-  brew install "$cask"
+  brew install --cask "$cask"
 done
 
 # Remove outdated versions from the cellar
