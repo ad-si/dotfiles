4521a4995d6683705688f6b7f5558b4355db9090
Adrian Sieber <mail@adriansieber.com>
2025-06-15 08:24:31 +0000

    Improve several commands, delete obsolete commands

--- bin/setup-homebrew.sh
+++ bin/setup-homebrew.sh
@@ -38,113 +38,11 @@ brew install bash
 
 # Miscellaneous
 brews=(
-  # ag
-  # aichat
-  # asciinema
-  # atuin
-  # awscli
-  bat
-  bun  # JavaScript engine
-  # colordiff
-  # csvkit  # Utilities for converting to and working with CSV
-  # deno  # JavaScript runtime
-  diskus
-  dockutil
-  # doggo  # DNS lookup utility
-  # duckdb  # In-memory SQL database system
-  exiftool  # Manage meta information of vaious files
-  eza
-  fastfetch  # Display system information in the terminal
-  fclones  # Find duplicate files
-  fd
-  # fdupes  # Find duplicate files (Use fclones instead)
-  fish
-  flyctl  # Fly.io CLI
-  fnm  # Fast Node.js version manager
-  # fzf  # Fuzzy finder
-  # gcal  # TODO: Re-add after the formula was re-activated on homebrew
-  gh
-  git
-  git-extras
-  # gitbatch  # Tool to manage multiple git repositories
-  # gleam  # Gleam programming language
-  gnu-sed
-  # gnuplot  # A graph plotter with support for various formats
-  # go  # Go programming language
-  # goaccess  # Web log analyzer
-  gopass
-  graphviz
-  # gum  # Utilities to write shell scripts
   hammerspoon  # Desktop automation
-  # hexyl  # Hex viewer
-  # heynote  # Note-taking app
-  htop
-  httpie
-  # hunspell
-  # hurl  # HTTP client and testing tool
-  # hyperfine  # Benchmarking tool
-  imagemagick
-  jo  # JSON output from a shell
-  jq  # JSON processor
-  just  # Command runner
-  kalker
-  kondo  # Clean dependencies and build artifacts
-  # kubectl
-  # lilypond  # Music notation software
-  lychee  # Link checker
-  mas  # Mac App Store command line interface
-  mdbook # Create books from markdown files
-  # micro
-  # mitmproxy  # Intercept, modify, replay, or save HTTP traffic
-  # mongodb
-  # mosh  # Mobile shell
-  mpv  # Media player
-  # node
-  numbat  # Calculator with support for units
-  # ocrmypdf  # Add an OCR text layer to scanned PDF files
-  ollama
-  # optipng
-  ocrmypdf  # Add an OCR text layer to scanned PDF files
-  openjdk  # Java development kit (Necessary for many programs)
-  optipng  # PNG optimizer
-  # osx-cpu-temp
-  pandoc
-  # pandoc-citeproc
-  # perspec  # Extract and perspectively correct document images
   pinentry-mac  # Needed for gopass
-  pkg-config  # Necessary for stack
-  # platypus  # CLI tool only (GUI gets installed with brew cask)
-  poppler  # PDF rendering library (Necessary for `rga`)
-  rclone  # Rsync for cloud storage
-  rga  # Ripgrep wrapper for PDFs, E-Books, Office documents, zip, tar.gz, etc.
-  ripgrep
-  # ruby  # Programming language
-  # s3cmd  # Command-line tool for the Amazon S3 service
-  sd  # Intuitive find & replace CLI
-  # shellcheck  # Linter for bash scripts
-  stack  # Haskell tool stack
-  sqlite  # SQLite database
-  tcl-tk # Tcl/Tk needed for pdfCropMargins
-  tealdeer  # Fast implementation of tldr in Rust
-  # tectonic # LaTeX engine
-  # tokei  # Display statistics about code
-  topgrade
-  trash
-  translate-shell
-  tree
-  # tesseract  # OCR (Optical Character Recognition) tool
-  # tesseract-lang  # All supported languages
-  typos-cli  # Find typos in text
-  typst  # Markup based typesetting system
-  # uchardet  # Encoding detection library
-  uv  # Python package manager
-  viu  # Terminal image viewer
-  # vlang  # V programming language
-  watchexec  # Executes commands in response to file modifications
+  tcl-tk  # Tcl/Tk needed for pdfCropMargins
+  trash  # Move files to trash
   wolfram-engine  # Execute Wolfram Language code
-  xsv  # CSV command line toolkit written in Rust
-  # zola  # Static site generator
-  zoxide  # Jump to directories
 )
 
 for brew in "${brews[@]}"
@@ -180,6 +78,7 @@ casks=(
   # cloudcompare  # FIXME: missing
   # cyberduck
   # db-browser-for-sqlite@nightly
+  # dbgate  # SQL database client
   # dedrm  # FIXME: missing
   # defold  # Game engine with Lua scripting
   # diffusionbee
@@ -202,6 +101,7 @@ casks=(
   font-hasklug-nerd-font
   # font-liberation
   # forklift  # File manager
+  freecad  # 3D CAD software
   # frescobaldi  # LilyPond music notation editor
   gimp
   # gitup
@@ -233,6 +133,7 @@ casks=(
   notion
   # nvidia-geforce-now
   # onyx
+  openscad  # 3D CAD software
   # opera
   # platypus
   qlstephen  # Preview plain text files without a file extension
@@ -240,6 +141,8 @@ casks=(
   # rawtherapee
   # send-to-kindle
   # sigil
+  signal  # Messaging app
+  # silicon  # Create images of your source code
   # silverlight
   # sketch
   # sketchup
@@ -254,6 +157,7 @@ casks=(
   sublime-merge
   sublime-text
   # sweet-home3d  # Interior design software
+  tableplus  # Database management app
   # table-tool
   # tad
   # teamviewer  # Remote desktop software
